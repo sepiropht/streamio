df44f4e8fb313874e1a6769dbee7b2f9dc49c31f
William Mbotta <sergembotta@mailoo.org>
2020-10-31 01:56:45 +0100

    upload files to s3

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,21 @@
 import axios from "axios";
+import { useState } from "react";
 import Layout from "../components/Layout";
 
 const IndexPage = () => {
-  function onChange(event: any) {
+  const [urls, setUrl] = useState<string[]>([]);
+  async function onChange(event: any) {
     console.log(event.target.files[0]);
-    axios.post("/video", {
-      video: event.target.files[0],
+    var formData = new FormData();
+    formData.append("video", event.target.files[0]);
+    const res = await axios.post("/api/video", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     });
+    setUrl([...urls, res.data.url]);
   }
+  const ListUrls = urls.map((url, index) => <li key={index}>{url}</li>);
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <input
@@ -16,6 +24,7 @@ const IndexPage = () => {
         placeholder="paste video url"
         onChange={onChange}
       />
+      <ul>{ListUrls}</ul>
     </Layout>
   );
 };
