bd2edaad56b2bf8575b24a834507bfe06a3821ae
William Mbotta <sergembotta@mailoo.org>
2020-11-01 16:14:03 +0100

    fetch all info from mongodb before fetch video

--- web/api/models.ts
+++ web/api/models.ts
@@ -1,15 +1,25 @@
-import { Schema, model, Document } from "mongoose";
+import mongoose, { Schema, model, Document } from "mongoose";
+
+const dbName = "test";
+const url = `mongodb+srv://admin:admin@cluster0.vod8r.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 interface IVideo {
   uuid: string;
+  extention: string;
   name: string;
 }
 const videoSchemaField: Record<keyof IVideo, any> = {
   uuid: String,
+  extention: String,
   name: String,
 };
 interface IVideoDoc extends IVideo, Document {}
 const videoSchema = new Schema(videoSchemaField);
 const Video = model<IVideoDoc>("video", videoSchema);
 
-export default Video;
+(async () => {
+  await mongoose.connect(url, { poolSize: 10, useNewUrlParser: true });
+  mongoose.connection;
+})();
+
+export { Video };
