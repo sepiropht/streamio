133a3b7c098a953be93b7402202879563207312b
William Mbotta <sergembotta@mailoo.org>
2020-11-01 16:14:03 +0100

    fetch all info from mongodb before fetch video

--- web/api/fetchVideo.ts
+++ web/api/fetchVideo.ts
@@ -1,23 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import streamify from "../utils/streamify";
 import s3 from "../utils/aws";
-
+import { Video } from "./models";
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     console.log(_req.query);
     const { uuid } = _req.query;
-    const Key = uuid + ".mkv";
-    console.log({ Key });
-
-    s3.getObject({ Bucket: "streamio/Test", Key }, (err: any, data: any) => {
-      if (err) return console.log(err);
-      console.log(
-        "downlaod successsss",
-        //Object.keys(data).map((key) => key),
-        data.Body
-      );
-      streamify(data.Body, _req, res);
-    });
+    let video;
+    if (typeof uuid === "string") {
+      video = await Video.findOne({ uuid });
+      console.log({ video });
+      const Key = uuid + video?.extention;
+      console.log({ Key });
+      s3.getObject({ Bucket: "streamio/test", Key }, (err: any, data: any) => {
+        if (err) return console.log(err);
+        console.log("downlaod successsss", data.Body);
+        streamify(data.Body, _req, res);
+      });
+    } else {
+      throw "query is not a string";
+    }
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message });
   }
--- web/api/video.ts
+++ web/api/video.ts
@@ -1,23 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import multiparty from "multiparty";
 import fs from "fs";
-import mongoose from "mongoose";
-import Video from "./models";
+import { Video } from "./models";
 import path from "path";
 import s3 from "../utils/aws";
 
 //const isProd = process.env.NODE_ENV === "production";
-const dbName = "test";
-const suffix = "Test";
+
+const suffix = "test";
 
 const Bucket = `streamio/${suffix}`;
-const url = `mongodb+srv://admin:admin@cluster0.vod8r.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (_req.method === "POST") {
-      await mongoose.connect(url, { poolSize: 10, useNewUrlParser: true });
-      mongoose.connection;
       let form = new multiparty.Form();
       form.parse(_req, async (_, __, files) => {
         const video = files.video[0];
@@ -34,12 +30,14 @@ const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
           }
           if (data) {
             const name = path.parse(video.originalFilename).name;
+            const uuid = path.parse(Key).name;
             await Video.create({
-              uuid: Key,
+              uuid,
+              extention: path.extname(Key),
               name,
             });
             console.log("Upload Success", data.Location);
-            res.status(200).json({ uuid: path.parse(Key).name, name });
+            res.status(200).json({ uuid, name });
           }
         });
       });
