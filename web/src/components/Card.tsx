dceadb054e2b540055a5e802a1e78b3fc0bba9f9
William Mbotta <sergembotta@mailoo.org>
2020-11-08 07:56:22 +0100

    play button

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Image, Textarea, Button } from "@chakra-ui/core";
+import { useState } from "react";
 import NextLink from "next/link";
 interface CardProps {
   src: string;
@@ -15,10 +16,47 @@ export const Card: React.FC<CardProps> = ({
 }) => {
   src =
     "https://cdn-cf-east.streamable.com/image/qzpehe.jpg?Expires=1604878380&Signature=kBqTh6VhDTvqdOBxWUOh0Can8JoGRkT1prg-9fo4zp0FOScNSzI2kB-tYXvwlwCNBSEAawQ99Es~3X3IdwBJZXs49NynNF198O66j1zUJBznOt12Cf-VTKl0fdp9ZMSq3T3Pqn6RaFbe1JwqU2AqMhAC2d577iqGj1qw18syO4VofN2zJj~qch6Ljjw9mq~Wvvfw1JZTNr6212jNbwgykdpgA5FoVH-c8~~FQl3EBEYz2h1WW0CwDaymYSlnR12aH9X8Q96cG17A3oQUWIOkcoQmep6fL6MBQZd0UozwnNtAzsoRYyZ8OgN~L~kbzb3YuoUUGaGx4m1CK~DAj-1sXA__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ";
+  const [isHover, setHover] = useState(false);
   return (
     <Box bg="white" border="1px solid #e8e8e8" border-radius="2px">
-      <Box width="100%">
-        <Box cursor="pointer"></Box>
+      <Box
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        width="100%"
+        position="relative"
+      >
+        <Box
+          className="play-button"
+          cursor="pointer"
+          position="absolute"
+          color="hsla(0,0%,100%,.9)"
+          top="0"
+          bottom="0"
+          left="0"
+          right="0"
+          textAlign="center"
+          fontSize="30px"
+          style={{
+            visibility: isHover ? "visible" : "hidden",
+            filter: "drop-shadow(0 0 3px rgba(0,0,0,.8))",
+          }}
+          alignItems="center"
+          justifyContent="center"
+          display="flex"
+        >
+          <svg
+            fill="currentColor"
+            preserveAspectRatio="xMidYMid meet"
+            height="1em"
+            width="1em"
+            viewBox="0 0 40 40"
+            style={{ verticalAlign: "middle" }}
+          >
+            <g>
+              <path d="m35.4 20.7l-29.6 16.5q-0.6 0.3-0.9 0t-0.4-0.8v-32.8q0-0.6 0.4-0.8t0.9 0l29.6 16.5q0.5 0.3 0.5 0.7t-0.5 0.7z"></path>
+            </g>
+          </svg>
+        </Box>
         <Image
           objectFit="cover"
           minHeight="160px"
@@ -46,7 +84,7 @@ export const Card: React.FC<CardProps> = ({
           minHeight="unset"
           marginBottom="7px"
         ></Textarea>
-        <Flex alignItems="start" justifyContent="space-between">
+        <Flex alignItems="start" justifyContent="flex-start">
           <NextLink href="/">
             <Box color="#007bff">https://streamio.com/qzpehe</Box>
           </NextLink>
@@ -55,6 +93,7 @@ export const Card: React.FC<CardProps> = ({
             bg="#748490"
             color="white"
             alignSelf="center"
+            marginLeft="10px"
             padding="0px 9px 0px 15px"
             fontSize="10px"
             lineHeight={1.5}
