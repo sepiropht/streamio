815427c861f7ae1a8c6809e903f1b2eb9d5c101a
William Mbotta <sergembotta@mailoo.org>
2020-11-15 19:29:26 +0100

    menu and page for profile and setting

---
+++ web/src/components/NavMenu.tsx
@@ -0,0 +1,114 @@
+import { Box, Flex, Image, Textarea, Button, Spinner } from "@chakra-ui/core";
+import { useState, useEffect } from "react";
+import NextLink from "next/link";
+import { useApolloClient } from "@apollo/client";
+import { ImShare2 } from "react-icons/im";
+import { ModalPlayer } from "./ModalPlayer";
+import { useLogoutMutation } from "../generated/graphql";
+
+interface NavMenuProps {
+  name: string;
+  activateOver?: boolean;
+}
+export const NavMenu: React.FC<NavMenuProps> = ({ name }) => {
+  const [logout, { loading: logoutFetching }] = useLogoutMutation();
+  const apolloClient = useApolloClient();
+  const [isShow, showMenu] = useState(false);
+  return (
+    <Box onClick={() => showMenu(isShow ? false : true)}>
+      <MenuHeader name={name} activateOver={true}></MenuHeader>
+      <Box
+        position="absolute"
+        right="16px"
+        top="8px"
+        bg="#fff"
+        border={isShow ? "1px solid #eee" : ""}
+        borderRadius="5px"
+        zIndex={90}
+        minWidth="180px"
+        maxWidth="300px"
+        boxShadow="0 0 5px rgba(0,0,0,.05)"
+        userSelect="none"
+        onClick={() => showMenu(isShow ? false : true)}
+      >
+        <Box display={isShow ? "block" : "none"}>
+          <MenuHeader name={name} activateOver={false}></MenuHeader>
+          <Box
+            fontSize="11px"
+            display={isShow ? "block" : "none"}
+            padding="8px 10px 3px"
+            fontWeight={600}
+            borderTop="1px solid #eee"
+            color="#748490"
+            textAlign="left"
+            lineHeight={1.5}
+          >
+            <Box>MY SETTINGS</Box>
+            <NextLink href="/profile">
+              <Box
+                display="block"
+                padding="7px 10px"
+                color="#333"
+                fontSize="14px"
+                fontWeight={600}
+                cursor="pointer"
+                textAlign="left"
+              >
+                My Profile
+              </Box>
+            </NextLink>
+            <Box></Box>
+            <Box borderTop="1px solid #eee">
+              <Button
+                textAlign="left"
+                color="black"
+                onClick={async () => {
+                  await logout();
+                  await apolloClient.resetStore();
+                }}
+                isLoading={logoutFetching}
+                variant="ghost"
+              >
+                Log out
+              </Button>{" "}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+interface MenuHeaderProps {
+  name: string;
+  activateOver: boolean;
+}
+export const MenuHeader: React.FC<MenuHeaderProps> = ({
+  name,
+  activateOver,
+}) => {
+  const [isHover, setHover] = useState(false);
+  return (
+    <Box
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      padding="3px 28px 3px 7px"
+      borderRadius="5px"
+      lineHeight="25px"
+      textAlign="right"
+      position="relative"
+      cursor="pointer"
+      color="#333"
+      fontWeight={600}
+      bg="transparent"
+      border="1px solid transparent"
+      margin="0 6px"
+      style={{
+        border: isHover && activateOver ? "1px solid #eee" : "",
+      }}
+      mr={2}
+    >
+      {name}
+    </Box>
+  );
+};
---
+++ web/src/pages/profile.tsx
@@ -0,0 +1,55 @@
+//import axios from "axios";
+import { useState } from "react";
+import { Layout } from "../components/Layout";
+import Link from "next/link";
+import { useUploadVideoMutation } from "../generated/graphql";
+import { Video } from "../interfaces";
+import { withApollo } from "../utils/withApollo";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { Button, Box, Flex, Grid } from "@chakra-ui/core";
+import { NavBar } from "../components/NavBar";
+import NextLink from "next/link";
+
+const Profile = () => {
+  const { data, loading } = useMeQuery({
+    skip: isServer(),
+  });
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <Box padding="30px">
+        <Box bg="white" fontWeight={400} fontSize="35px" width="100%">
+          <h1>Profile</h1>
+        </Box>
+        <Box fontWeight={500} color={"rgb(116, 132, 144)"}>
+          Contact info, view preferences, and account overview.
+        </Box>
+        <Flex
+          border-top="1px solid #eee"
+          margin-top="30px"
+          alignItems="center"
+          padding-top="30px"
+        >
+          <Box>
+            <Box>Login</Box>
+            <Box>{data?.me?.username} Joined()</Box>
+          </Box>
+          <Box>
+            <NextLink href="/forgot-password">
+              <Box textAlign="center" color="#1a70b9">
+                Change password
+              </Box>
+            </NextLink>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default withApollo({ ssr: false })(Profile);
