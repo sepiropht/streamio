ebb61cb31d9a4490b5fe3cc8cfa69e9db17bf63f
William Mbotta <sergembotta@mailoo.org>
2020-12-03 08:10:14 +0100

    create dev environnement

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,6 +3,14 @@
 echo What should the version be?
 read VERSION
 
-docker build -t sepiropht/lireddit:$VERSION .
-docker push sepiropht/lireddit:$VERSION
-ssh root@104.248.38.40 "docker pull sepiropht/lireddit:$VERSION && docker tag sepiropht/lireddit:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+
+if [ ${ENV} = "PROD" ]; then 
+    docker build -t sepiropht/lireddit:$VERSION .
+    docker push sepiropht/lireddit:$VERSION
+    ssh root@104.248.38.40 "docker pull sepiropht/lireddit:$VERSION && docker tag sepiropht/lireddit:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+else 
+    docker build -t sepiropht/lireddit:$VERSION -f Dockerfile.dev .
+    docker push sepiropht/lireddit:$VERSION
+    ssh root@104.248.38.40 "docker pull sepiropht/lireddit:$VERSION && docker tag sepiropht/lireddit:$VERSION dokku/streamio-dev:$VERSION && dokku deploy streamio-dev $VERSION"
+fi
+    
