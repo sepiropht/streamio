e8e1ba4837f497bde63372fe9d3ed90f2f4b9dd2
William Mbotta <sergembotta@mailoo.org>
2020-12-30 03:00:27 +0100

    domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -116,7 +116,7 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
-        sameSite: "lax", // csrf
+        sameSite: "none", // csrf
         secure: __prod__, // cookie only works in https
         domain: __prod__ ? ".sepiropht.com" : undefined,
       },
