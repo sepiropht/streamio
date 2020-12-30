26974263fe3d31cab7e83e0fcce2b2239a71c5ef
William Mbotta <sergembotta@mailoo.org>
2020-12-30 03:44:19 +0100

    no upgrade yet

--- web/src/pages/create-post.tsx
+++ web/src/pages/create-post.tsx
@@ -14,46 +14,7 @@ const CreatePost: React.FC<{}> = ({}) => {
   const router = useRouter();
   useIsAuth();
   const [createPost] = useCreatePostMutation();
-  return (
-    <Layout variant="small">
-      <Formik
-        initialValues={{ title: "", text: "" }}
-        onSubmit={async (values) => {
-          const { errors } = await createPost({
-            variables: { input: values },
-            update: (cache) => {
-              cache.evict({ fieldName: "posts:{}" });
-            },
-          });
-          if (!errors) {
-            router.push("/");
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name="title" placeholder="title" label="Title" />
-            <Box mt={4}>
-              <InputField
-                textarea
-                name="text"
-                placeholder="text..."
-                label="Body"
-              />
-            </Box>
-            <Button
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              colorScheme="teal"
-            >
-              create post
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Layout>
-  );
+  return <Layout variant="small">Not available yet</Layout>;
 };
 
 export default withApollo({ ssr: false })(CreatePost);
