c082fb206e568e48534b2bd73c6f1dc19970a959
William Mbotta <sergembotta@mailoo.org>
2022-04-04 21:53:01 +0200

    remove streamify

--- server/src/utils/streamify.ts
+++ server/src/utils/streamify.ts
@@ -6,36 +6,37 @@ export default async function (
   res: any,
   fileSize: number
 ) {
-  const range = req.headers.range
+  console.log(req, fileSize)
+  // const range = req.headers.range
 
-  console.log({ fileSize })
+  // console.log({ fileSize })
 
-  if (range) {
-    const parts = range.replace(/bytes=/, '').split('-')
+  // if (range) {
+  //   const parts = range.replace(/bytes=/, '').split('-')
 
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+  //   const start = parseInt(parts[0], 10)
+  //   const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
 
-    const chunksize = end - start + 1
+  //   const chunksize = end - start + 1
 
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    }
+  //   const head = {
+  //     'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+  //     'Accept-Ranges': 'bytes',
+  //     'Content-Length': chunksize,
+  //     'Content-Type': 'video/mp4',
+  //   }
 
-    console.log('yeah accept range')
-    res.writeHead(200, head)
-    videoStream.pipe(res)
-  } else {
-    console.log('without range')
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-  }
+  //   console.log('yeah accept range')
+  //   res.writeHead(200, head)
+  //   videoStream.pipe(res)
+  // } else {
+  //   console.log('without range')
+  //   const head = {
+  //     'Content-Length': fileSize,
+  //     'Content-Type': 'video/mp4',
+  //   }
+  //   res.writeHead(200, head)
+  // }
 
   videoStream.pipe(res)
 }
