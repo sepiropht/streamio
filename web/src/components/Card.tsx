b0e1385c47d6e49239c778ad18cec4e9b137a40b
William Mbotta <sergembotta@mailoo.org>
2022-04-06 21:41:20 +0200

    fix style

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,28 +1,28 @@
-import { Box, Flex, Image, Textarea, Button } from "@chakra-ui/react";
-import { useState, useEffect, useRef } from "react";
-import NextLink from "next/link";
-import { ImShare2 } from "react-icons/im";
-import { ModalPlayer } from "./ModalPlayer";
-import { BsThreeDots } from "react-icons/bs";
-import { RiDeleteBinFill } from "react-icons/ri";
-import { useDeleteVideoMutation } from "../generated/graphql";
-import { useCopyToClipboard } from "react-use";
-import { useRouter } from "next/router";
+import { Box, Flex, Image, Textarea, Button } from '@chakra-ui/react'
+import { useState, useEffect, useRef } from 'react'
+import NextLink from 'next/link'
+import { ImShare2 } from 'react-icons/im'
+import { ModalPlayer } from './ModalPlayer'
+import { BsThreeDots } from 'react-icons/bs'
+import { RiDeleteBinFill } from 'react-icons/ri'
+import { useDeleteVideoMutation } from '../generated/graphql'
+import { useCopyToClipboard } from 'react-use'
+import { useRouter } from 'next/router'
 
 interface CardProps {
-  Key: string;
-  src: string;
-  useWebSocket?: boolean;
-  views: number;
-  link: string;
-  title: string;
-  socialUrl?: any;
-  upload?: any;
-  onDeletedCard: (currentId: number) => void;
-  videoUrl: string;
-  isCardLoaded: boolean;
-  progress?: number;
-  id: number;
+  Key: string
+  src: string
+  useWebSocket?: boolean
+  views: number
+  link: string
+  title: string
+  socialUrl?: any
+  upload?: any
+  onDeletedCard: (currentId: number) => void
+  videoUrl: string
+  isCardLoaded: boolean
+  progress?: number
+  id: number
 }
 export const Card: React.FC<CardProps> = ({
   id,
@@ -37,83 +37,82 @@ export const Card: React.FC<CardProps> = ({
   Key,
   upload,
 }) => {
-  const [isHover, setHover] = useState(false);
-  const [isVisible, showModal] = useState(false);
-  const [currentProgress, setProgress] = useState<number | undefined>(
-    undefined
-  );
-  const [task, setTask] = useState("");
-  const router = useRouter();
-  const [video, setVideoUrl] = useState("");
-  const imageElement = useRef<HTMLImageElement>(null);
-  const [isMenuShow, showMenu] = useState(false);
-  const [deleteVideo] = useDeleteVideoMutation();
-  const [_, copyToClipboard] = useCopyToClipboard();
-  const ws = useRef<WebSocket>();
+  const [isHover, setHover] = useState(false)
+  const [isVisible, showModal] = useState(false)
+  const [currentProgress, setProgress] = useState<number | undefined>(undefined)
+  const [task, setTask] = useState('')
+  const router = useRouter()
+  const [video, setVideoUrl] = useState('')
+  const imageElement = useRef<HTMLImageElement>(null)
+  const [isMenuShow, showMenu] = useState(false)
+  const [deleteVideo] = useDeleteVideoMutation()
+  const [_, copyToClipboard] = useCopyToClipboard()
+  const ws = useRef<WebSocket>()
 
   useEffect(() => {
     if (!useWebSocket) {
-      return;
+      return
     }
     const websocketPrefix =
-      process.env.NEXT_PUBLIC_PROD === "production" ? "wss:" : "ws:";
-    ws.current = new WebSocket(websocketPrefix + process.env.NEXT_PUBLIC_URL);
-    ws.current.onclose = () => console.log("ws closed");
-    ws.current.onopen = () => socialUrl && socialUrl(ws);
+      process.env.NEXT_PUBLIC_PROD === 'production' ? 'wss:' : 'ws:'
+    ws.current = new WebSocket('ws://localhost:4000')
+    console.log(process.env.NEXT_PUBLIC_URL)
+    ws.current.onclose = () => console.log('ws closed')
+    ws.current.onopen = () => socialUrl && socialUrl(ws)
     ws.current.onmessage = ({ data }) => {
-      const res = JSON.parse(data);
+      const res = JSON.parse(data)
       if (res.delete && res.key === Key) {
-        onDeletedCard(id);
+        onDeletedCard(id)
       }
       if (res.progress && res.Key && Key === res.Key) {
-        setTask("Processing");
-        setProgress(parseInt(res.progress.percent, 10));
+        setTask('Processing')
+        setProgress(parseInt(res.progress.percent, 10))
       }
 
       if (res.imageReady && res.Key === Key) {
-        if (imageElement?.current) imageElement.current.src = src;
+        if (imageElement?.current) imageElement.current.src = src
       }
       if (res.done && Key === res.Key) {
-        setProgress(undefined);
-        setVideoUrl(videoUrl);
+        setProgress(undefined)
+        setVideoUrl(videoUrl)
       }
-    };
+    }
     // ws.current.onmessage = ({ data }) => {
     //   console.log(data);
     // };
 
-    return () => ws.current?.close();
-  }, [useWebSocket]);
+    return () => ws.current?.close()
+  }, [useWebSocket])
 
   useEffect(() => {
     upload
       ? upload(async (err: any, res: any) => {
-          if (err) return console.log("EEEEEEEEEEEEEEEEEEEEERRR", err);
+          if (err) return console.log('EEEEEEEEEEEEEEEEEEEEERRR', err)
           ws?.current?.send(
             JSON.stringify({
-              processVideo: "",
+              processVideo: '',
               key: Key,
             })
-          );
+          )
         }).on(
-          "httpUploadProgress",
+          'httpUploadProgress',
           ({ loaded, total }: { loaded: number; total: number }) => {
-            setProgress(loaded === total ? 100 : (loaded / total) * 100);
+            setProgress(loaded === total ? 100 : (loaded / total) * 100)
           }
-        ) && setTask("Uploading")
-      : "";
-  }, []);
+        ) && setTask('Uploading')
+      : ''
+  }, [])
 
   useEffect(() => {
     function setCard() {
-      setVideoUrl(videoUrl);
-      if (imageElement?.current) imageElement.current.src = src;
+      setVideoUrl(videoUrl)
+      if (imageElement?.current) imageElement.current.src = src
     }
-    if (!useWebSocket) setCard();
-  }, []);
+    if (!useWebSocket) setCard()
+  }, [])
 
   interface MenuCardProps {
-    show: boolean;
+    show: boolean
   }
   const MenuCard: React.FC<MenuCardProps> = ({ show }) => {
     return (
@@ -128,26 +127,26 @@ export const Card: React.FC<CardProps> = ({
         right="-16px;"
         padding="3px"
         top="268px"
-        display={show ? "flex" : "none"}
+        display={show ? 'flex' : 'none'}
       >
         <ActionMenu
           action={() => {
-            onDeletedCard(id);
+            onDeletedCard(id)
             deleteVideo({
               variables: { id },
               // update: (cache) => {
               //   // Post:77
               //   cache.evict({ id: "Post:" + id });
               // },
-            });
+            })
           }}
           name="Delete"
         >
           <RiDeleteBinFill></RiDeleteBinFill>
         </ActionMenu>
       </Box>
-    );
-  };
+    )
+  }
 
   return (
     <>
@@ -161,22 +160,23 @@ export const Card: React.FC<CardProps> = ({
         border="1px solid #e8e8e8"
         position="relative"
         border-radius="2px"
+        maxWidth="300px"
       >
         <Box
-          onMouseEnter={() => (currentProgress ? "" : setHover(true))}
-          onMouseLeave={() => (currentProgress ? "" : setHover(false))}
+          onMouseEnter={() => (currentProgress ? '' : setHover(true))}
+          onMouseLeave={() => (currentProgress ? '' : setHover(false))}
           width="100%"
           position="relative"
         >
           <Box
-            display={currentProgress ? "block" : "none"}
+            display={currentProgress ? 'block' : 'none'}
             verticalAlign="center"
           >
             <ProgressBar task={task} progress={currentProgress}></ProgressBar>
           </Box>
           <Box
             className="play-button"
-            onClick={() => (currentProgress ? "" : showModal(true))}
+            onClick={() => (currentProgress ? '' : showModal(true))}
             cursor="pointer"
             position="absolute"
             color="hsla(0,0%,100%,.9)"
@@ -187,8 +187,8 @@ export const Card: React.FC<CardProps> = ({
             textAlign="center"
             fontSize="30px"
             style={{
-              visibility: isHover ? "visible" : "hidden",
-              filter: "drop-shadow(0 0 3px rgba(0,0,0,.8))",
+              visibility: isHover ? 'visible' : 'hidden',
+              filter: 'drop-shadow(0 0 3px rgba(0,0,0,.8))',
             }}
             alignItems="center"
             justifyContent="center"
@@ -200,7 +200,7 @@ export const Card: React.FC<CardProps> = ({
               height="1em"
               width="1em"
               viewBox="0 0 40 40"
-              style={{ verticalAlign: "middle" }}
+              style={{ verticalAlign: 'middle' }}
             >
               <g>
                 <path d="m35.4 20.7l-29.6 16.5q-0.6 0.3-0.9 0t-0.4-0.8v-32.8q0-0.6 0.4-0.8t0.9 0l29.6 16.5q0.5 0.3 0.5 0.7t-0.5 0.7z"></path>
@@ -284,7 +284,7 @@ export const Card: React.FC<CardProps> = ({
             lineHeight="24px;"
             fontSize="13px"
             variant="link"
-            display={!currentProgress ? "flex" : "none"}
+            display={!currentProgress ? 'flex' : 'none'}
             justifyContent="space-between"
             onClick={() => showMenu(isMenuShow ? false : true)}
           >
@@ -297,13 +297,13 @@ export const Card: React.FC<CardProps> = ({
             padding="0 15"
             fontWeight="bold"
             borderRadius="3px"
-            display={!currentProgress ? "flex" : "none"}
+            display={!currentProgress ? 'flex' : 'none'}
             height="26px"
             lineHeight="24px;"
             fontSize="13px"
             variant="link"
             justifyContent="space-between"
-            onClick={() => router.push({ pathname: "/edit", query: { Key } })}
+            onClick={() => router.push({ pathname: '/edit', query: { Key } })}
           >
             Edit
           </Button>
@@ -311,12 +311,12 @@ export const Card: React.FC<CardProps> = ({
         <MenuCard show={isMenuShow}></MenuCard>
       </Box>
     </>
-  );
-};
+  )
+}
 
 interface ActionMenuProps {
-  name: string;
-  action: () => void;
+  name: string
+  action: () => void
 }
 const ActionMenu: React.FC<ActionMenuProps> = ({ name, action, children }) => (
   <Box
@@ -336,10 +336,10 @@ const ActionMenu: React.FC<ActionMenuProps> = ({ name, action, children }) => (
     <Box marginRight="8px">{children}</Box>
     {name}
   </Box>
-);
+)
 interface ProgressBarProps {
-  task: string;
-  progress?: number;
+  task: string
+  progress?: number
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ task, progress }) => (
@@ -388,4 +388,4 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ task, progress }) => (
       ></Box>
     </Box>
   </Box>
-);
+)
